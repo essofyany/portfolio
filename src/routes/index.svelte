df5560ea57d61c61e0119cfbe12cc4f931d75538
essofyany <soufyani.bilal@gmail.com>
2022-05-09 23:48:30 +0100

    create hero section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="absolute top-[18%] right-1/4 bottom-1/4 left-1/4 h-80 bg-light/0 flex flex-col gap-8">
+	<!-- avatar card -->
+	<div class="flex items-center gap-4">
+		<figure
+			class="w-20 h-20 rounded-full bg-gradient-to-tl from-gold to-meant flex justify-center items-center p-1"
+		>
+			<img class="rounded-full w-full h-full" src="/me.jpg" alt="bilal essofyany" />
+		</figure>
+		<div class="flex flex-col gap-1">
+			<div class="relative w-fit">
+				<div class="absolute bottom-1 w-full h-2 bg-meant -skew-x-12 -skew-y-1" />
+				<h1 class="relative z-10 text-light text-3xl font-bold">Essofyany Bilal</h1>
+			</div>
+			<p class="text-gold font-light">Full-stack developer (Heavy Frontend)</p>
+		</div>
+	</div>
+	<!-- bio -->
+	<div class="text-light/60 leading-relaxed tracking-wide text-sm flex flex-col gap-2 mt-5">
+		<p class="">
+			Thank you for a look in. I'm a Full Stack developer (Heavy Frontend). I love to write about
+			Javascript/Typescript, React/NextJS, Svelte/SvelteKit, and NodeJS/Express, I am also involved
+			in dev communities. I enjoy connecting with people to share ideas and knowledge. My strength
+			lies in a combination of self-learning and consistency.
+		</p>
+		<p class="">
+			To improve myself at every moment, I believe in the learning process rather than results.
+			Along with all of this, I am passionate about contributing to an open-source project.
+			Currently, I am contributing to react-awesome-shapes open-source community .
+		</p>
+	</div>
+	<!-- navs -->
+	<ul class="flex gap-24 text-black">
+		<li class="w-7 h-7 p-[7px] bg-gold rounded-lg flex items-center gap-3">
+			<img class="opacity-90" src="/icons/User.svg" alt="about me" />
+			<p class="text-gold font-medium">About</p>
+		</li>
+		<li class="w-7 h-7 p-[6px] bg-gold rounded-lg flex items-center gap-3">
+            <img class="opacity-90" src="/icons/Document.svg" alt="about me" />
+            <p class="text-gold font-medium">Posts</p>
+		</li>
+		<li class="w-7 h-7 p-1 bg-gold rounded-lg flex items-center gap-3">
+            <img class="opacity-90" src="/icons/Discovery.svg" alt="about me" />
+            <p class="text-gold font-medium">Projects</p>
+		</li>
+		<li class="w-7 h-7 p-1 bg-gold rounded-lg flex items-center gap-3">
+            <img class="opacity-90" src="/icons/Star.svg" alt="about me" />
+            <p class="text-gold font-medium">Resources</p>
+		</li>
+	</ul>
+</section>
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				gold: '#E9C46A',
+				light: '#F7F9F9',
+				black: '#070F12',
+				dark: '#264653',
+				meant: '#2A9D8F',
+				lightRed: '#E76F51',
+				lightOrange: '#F4A261'
+			}
+		}
+	},
+	plugins: []
+};
