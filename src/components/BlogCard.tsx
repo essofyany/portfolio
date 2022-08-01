ec8d38830db25f49a73efe79f92cc2a4e4ba99a7
essofyany <soufyani.bilal@gmail.com>
2022-08-01 22:06:23 +0100

    create blog card skeleton

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,12 @@
+export default function BlogCard() {
+	return (
+		<div class='space-y-5 rounded-2xl bg-white/10 p-4'>
+			<div class='h-24 rounded-lg bg-white/25'></div>
+			<div class='space-y-3'>
+				<div class='h-3 w-3/5 rounded-lg bg-white/10'></div>
+				<div class='h-3 w-4/5 rounded-lg bg-white/20'></div>
+				<div class='h-3 w-2/5 rounded-lg bg-white/20'></div>
+			</div>
+		</div>
+	);
+}
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,3 +1,19 @@
+// import BlogCard from '../constants/BlogCard';
+import BlogSkeleton from '../components/BlogCardSkeleton';
+
 export default function BlogPage() {
-	return <div class='w-full py-8 __mainContent'></div>;
+	return (
+		<div class='w-full py-8 __mainContent'>
+			<div class='w-full grid grid-cols-2 gap-4'>
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+				<BlogSkeleton />
+			</div>
+		</div>
+	);
 }
