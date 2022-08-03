b55d2f056c1d32c42bfa0886444b56b7f4f409c0
essofyany <soufyani.bilal@gmail.com>
2022-08-03 16:35:12 +0100

    fix glass card width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout(props: { children: JSX.Element }) {
 
   return (
     <div class="container h-full">
-      <div class="w-full md:w-2/3 lg:1/2 mx-auto h-full">
+      <div class="w-full md:w-1/2 lg:2/3 mx-auto h-full">
         <GlassCard>
           <Header />
           <main class="w-full">{c()}</main>
